c398d6e6140700dbaf34a9a1eb9bea5af0652db5
Jenny Sun <xmissj96@gmail.com>
2019-09-16 15:17:20 +0200

    Changes: Fixed the ViewHolder problem somehow.

--- app/src/main/java/com/example/jenny/swipequiz/QuestionAdapter.kt
+++ app/src/main/java/com/example/jenny/swipequiz/QuestionAdapter.kt
@@ -1,26 +1,45 @@
 package com.example.jenny.swipequiz
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class QuestionAdapter (private val questions: List<Question>) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
+// An adapter is created to know how and which items to display.
+class QuestionAdapter(private val questions: List<Question>) : RecyclerView.Adapter<QuestionAdapter.ViewHolder>() {
 
-    /**
-     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
-     */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+
+    // Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
         )
     }
 
-    private val tvQuestion: TextView = itemView.findViewById(android.R.id.text1)
-
-    fun bind(reminder: Question) {
-        tvQuestion.text = question.question
+    // Returns the size of the list
+    override fun getItemCount(): Int {
+        return questions.size
     }
 
 
+    // Called by RecyclerView to display the data at the specified position.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(questions[position])
+    }
+
+    // ViewHolder with a bind method which binds the Question String to a TextView.
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val tvQuestions: TextView = itemView.findViewById(android.R.id.text1)
+
+        fun bind(question: Question) {
+            tvQuestions.text = question.question
+        }
+
+    }
 }
+
+
+
